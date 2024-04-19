f7d313ded0c8445fa11e892a94cb8cfedb2aadb8
Maxime Pigeon <me@maximepigeon.com>
2024-04-19 14:54:11 -0400

    Fish: add fuzzy cd and fzf default options

--- fish/config.fish
+++ fish/config.fish
@@ -30,6 +30,24 @@ function ccd
     and cd $argv[1]
 end
 
+function cdf
+    cd $(fd -t directory | fzf --border-label=' Change Directory ')
+end
+
 function fish_prompt
     printf '%s' (basename $(prompt_pwd)) (fish_git_prompt) " \$ "
 end
+
+set -x FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix'
+set -x FZF_DEFAULT_OPTS "\
+--layout=reverse \
+--no-scrollbar \
+--color='\
+gutter:-1,\
+pointer:bright-white,\
+fg:bright-white,\
+hl:white,\
+hl+:white' \
+--prompt='' \
+--border=rounded \
+--no-info"
